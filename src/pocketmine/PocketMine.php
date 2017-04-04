2925c3a9c06b3dd18b27f7daea76d8a9157ad806
Apple <ytapplegamer@gmail.com>
2017-04-03 23:20:50 -0400

    Relocate installer files into locale
    and changed the version

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -74,8 +74,8 @@ namespace pocketmine {
     use pocketmine\utils\Utils;
     use pocketmine\wizard\Installer;
 
-    const VERSION = "1.2.1";
-    const API_VERSION = "3.0.1";
+    const VERSION = "2.0.0";
+    const API_VERSION = "3.0.0-ALPHA3";
     const CODENAME = "Opal";
 
     /*
