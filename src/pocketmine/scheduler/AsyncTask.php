ae2ae16e587017e8d28ce32bf5922c9f6a34cb85
LynxesMCPE <strikecounter27@gmail.com>
2017-05-28 14:20:04 +0800

    Fixed Duplicated Code
    Fixed undefined offset when retrieving something from the thread store that doesn't exist and duplicate code fixed.

--- src/pocketmine/scheduler/AsyncTask.php
+++ src/pocketmine/scheduler/AsyncTask.php
@@ -126,7 +126,6 @@ abstract class AsyncTask extends \Threaded implements \Collectable {
      */
     public function getFromThreadStore($identifier) {
         global $store;
-        return $this->isGarbage() ? null : $store[$identifier];
 		return ($this->isGarbage() or !isset($store[$identifier])) ? null : $store[$identifier];
     }
 
